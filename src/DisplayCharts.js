704b7b0e7ce46a62fad7e1be7f9b44f5d2d79570
Harshita Mathur <hmathur0797@gmail.com>
2021-08-17 11:06:27 +0530

    lint disabled

--- src/DisplayCharts.js
+++ src/DisplayCharts.js
@@ -10,6 +10,7 @@ export default function DisplayCharts() {
 
   useEffect(() => {
     getSevenDaysData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getSevenDaysData = () => {
